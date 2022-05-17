48ea25a5a0681e939b40dfbcdb847522049a484c
ThiSBernardo <93749791+ThiSBernardo@users.noreply.github.com>
2022-05-17 17:28:22 -0300

    Delete button

--- frontend/src/app/components/product/product.service.ts
+++ frontend/src/app/components/product/product.service.ts
@@ -11,9 +11,9 @@ export class ProductService {
 
   baseUrl = "http://localhost:3001/products"
 
-  constructor( private snackBar: MatSnackBar, private http: HttpClient) { }
+  constructor(private snackBar: MatSnackBar, private http: HttpClient) { }
   showMessage(msg: string): void {
-    this.snackBar.open(msg, 'X',{
+    this.snackBar.open(msg, 'X', {
       duration: 3000,
       horizontalPosition: "right",
       verticalPosition: "top",
@@ -36,5 +36,10 @@ export class ProductService {
   update(product: Product): Observable<Product> {
     const url = `${this.baseUrl}/${product.id}`
     return this.http.put<Product>(url, product)
+  }
+
+  delete(id: string): Observable<Product> {
+    const url = `${this.baseUrl}/${id}`;
+    return this.http.delete<Product>(url);
   }
 }
