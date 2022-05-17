45f9a2b312ab018833e8fe61c11632f7e6c7be77
ThiSBernardo <93749791+ThiSBernardo@users.noreply.github.com>
2022-05-17 14:09:46 -0300

    tabela de produtos

--- frontend/src/app/componets/product/product-read/product-read.component.ts
+++ frontend/src/app/componets/product/product-read/product-read.component.ts
@@ -10,6 +10,7 @@ import { ProductService } from 'src/app/components/product/product.service';
 export class ProductReadComponent implements OnInit {
 
   products: Product[]=[]
+  displayedColumns = ['id', 'name', 'price']
 
   constructor(private productService: ProductService) { }
 
